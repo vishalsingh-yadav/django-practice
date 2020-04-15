42df057bd16283efe38de1859c555dd5ea01e5d4
vishalsingh-yadav <63701529+vishalsingh-yadav@users.noreply.github.com>
2020-04-15 13:47:05 +0530

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '4dk$$=^y(hvjc84(s&&^%8a@&@b6$ir+ceoru^vk3bt3=m3k2x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vishalyadav.pythonanywhere.com']
 
 
 # Application definition
